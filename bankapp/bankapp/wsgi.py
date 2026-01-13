93ec15aa152513b4af81c5c4e53244f9bf47597d
Randy Mike <randymike926@gmail.com>
2026-01-13 18:31:50 +0200

    Fix Django settings module path for Render deployment

--- bankapp/bankapp/wsgi.py
+++ bankapp/bankapp/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bankapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bankapp.bankapp.settings')
 
 application = get_wsgi_application()
